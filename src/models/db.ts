e2ab203be7db677168f51d37a00a6711470f7b1b
Niv Hojberg <nivhojberg@hotmail.com>
2024-05-09 19:18:18 +0300

    remove unused comment

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-console.log("connect to mongo");
 mongoose.connect('mongodb://localhost:27017/VayuBackendTestDB').then(() => {
     console.log("Connection succeded");
 }).catch((err) => {
